b72ee2b8eba3d73bbfcb8e5d014b49ca99c31252
abhishekkumar <abhishekkumar0841@gmail.com>
2023-08-14 00:06:29 +0530

    project almost complete

--- reacttodoapp/src/components/MyTodoList.js
+++ reacttodoapp/src/components/MyTodoList.js
@@ -1,52 +1,70 @@
-import React, { useState } from 'react'
-import './MyTodoList.css'
+import React, { useState } from "react";
+import "./MyTodoList.css";
 
 function MyTodoList() {
+  const [inputValue, setInputValue] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [status, setStatus] = useState("Pending")
 
-    const [inputValue, setInputValue] = useState('')
-    const [todoList, setTodoList] = useState([])
+  function addTodo() {
+    setTodoList([...todoList, inputValue]);
+    setInputValue("");
+  }
 
-    function addTodo(){
-        setTodoList([...todoList, inputValue])
-        setInputValue('')
-    }
+  function removeTodo(idx) {
+    const updateTodo = todoList.filter((el, id) => {
+      return idx !== id;
+    });
+    setTodoList(updateTodo);
+  }
 
-    function removeTodo(idx){
-        const updateTodo = todoList.filter((el, id) => {
-            return idx !== id 
-        })
-        setTodoList(updateTodo)
-    }
+  function removeAllTodo() {
+    setTodoList([]);
+  }
 
-    function removeAllTodo(){
-        setTodoList([])
-    }
+  function updateStatus(){
+    setStatus("Completed")
+  }
 
   return (
-    <div className='todoContainer'>
-        <div className="todoInput">
-            <input type="text" className='input' placeholder='Enter Your Todo' value={inputValue} onChange={(e)=>setInputValue(e.target.value)} />
-            <button type='submit' className='todoInputBtn' onClick={addTodo}>Add</button>
-        </div>
+    <div className="todoContainer">
+      <div className="todoInput">
+        <input
+          type="text"
+          className="input"
+          placeholder="Enter Your Todo"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button type="submit" className="todoInputBtn" onClick={addTodo}>
+          Add
+        </button>
+      </div>
 
-        {
-            todoList !== [] && todoList.map((todo, idx)=>{
-                return (
-                    <div className='todoList'>
-                        <li key={idx}>
-                            {todo}
-                        </li>
-                        <button className='removeTodoBtn' onClick={()=>removeTodo(idx)}>Remove</button>
-                    </div>
-                )
-            }) 
-        }
+      {todoList !== [] &&
+        todoList.map((todo, idx) => {
+          // const [count, setCount] = useState(1)
+          return (
+            <div className="todoList">
+              <li key={idx} className="list">
+                <h2>{idx + 1 + ". " + todo}</h2>
+                <p className="status">Status: {status} </p>
+              </li>
+              <button className="updateStatusBtn" onClick={updateStatus}>Update Status</button>
+              <button className="removeTodoBtn" onClick={() => removeTodo(idx)}>
+                Remove
+              </button>
+            </div>
+          );
+        })}
 
-        {
-            todoList.length >= 2 && <button className='removeAllTodoBtn' onClick={removeAllTodo}>Remove All</button>
-        }
+      {todoList.length >= 2 && (
+        <button className="removeAllTodoBtn" onClick={removeAllTodo}>
+          Remove All
+        </button>
+      )}
     </div>
-  )
+  );
 }
 
-export default MyTodoList
+export default MyTodoList;
